ccd91c803b7681227724dd7bba59818c60686707
thannes30 <thannes30@yahoo.com>
2014-07-06 16:41:53 -0400

    added layout.erb

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require #to bring in all dependencies of gems
 
-require relative 'models/friend' #GET request to /friends
+# require relative 'models/friend' #GET request to /friends
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
